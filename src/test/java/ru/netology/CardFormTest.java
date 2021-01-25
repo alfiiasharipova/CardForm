eb9a8d55827f9a5a1c14231c00715f1c5b74e064
alfiiasharipova <alfiiasharipova@gmail.com>
2021-01-25 11:34:06 +0300

    Shift+Home added

--- src/test/java/ru/netology/CardFormTest.java
+++ src/test/java/ru/netology/CardFormTest.java
@@ -6,14 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardFormTest {
 
@@ -23,17 +30,27 @@ public class CardFormTest {
     }
 
     @Test
-    void shouldSubmitRequest() {
+    void shouldSubmitRequest() throws ParseException {
         $("[data-test-id=city] input").setValue("Казань");
         SelenideElement date = $("[data-test-id=date] input");
-        date.sendKeys(Keys.CONTROL, "A");
+        date.sendKeys(Keys.SHIFT, Keys.HOME);
         date.sendKeys(Keys.DELETE);
-        String newDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("ddMMyyyy"));
-        date.setValue(newDate);
+        LocalDate newDate = LocalDate.now().plusDays(4);
+        String newDateStr = newDate.format(DateTimeFormatter.ofPattern("ddMMyyyy"));
+        date.setValue(newDateStr);
         $("[name=name]").setValue("Кашин Василий");
         $("[name=phone]").setValue("+79270000000");
         $(".checkbox__box").click();
         $(".button").click();
         $(".notification").shouldBe(visible, Duration.ofSeconds(15));
+        Pattern pattern = Pattern.compile("\\d{2}.\\d{2}.\\d{4}");
+        Matcher matcher = pattern.matcher($(".notification__content").getText());
+        String actual = "";
+        if(matcher.find())
+            actual = matcher.group();
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date actualDate= format.parse(actual);
+        Date expectedDate = Date.from(newDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        assertEquals(expectedDate, actualDate);
     }
 }
